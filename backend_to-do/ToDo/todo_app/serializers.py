e1676bf4d01851d68dd31db817a7d38931092ded
neil <neilaronndiamzon@gmail.com>
2025-03-25 23:16:53 +1300

    registration, login, logout - done

--- backend_to-do/ToDo/todo_app/serializers.py
+++ backend_to-do/ToDo/todo_app/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import TodoCard
 
@@ -5,3 +6,14 @@ class TodoCardSerializer(serializers.ModelSerializer):
     class Meta:
         model = TodoCard
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
--- backend_to-do/ToDo/todo_app/urls.py
+++ backend_to-do/ToDo/todo_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
+from .views import RegisterView, LoginView, LogoutView
 from .viewsets import TodoCardViewSet
 
 router = DefaultRouter()
 router.register(r'todo', TodoCardViewSet)  # The API will be available at /api/todo/
 
-urlpatterns = [
-    path('api/', include(router.urls)),
-]
+urlpatterns = [path('api/', include(router.urls)), path('register/', RegisterView.as_view(), name='register'),
+               path('login/', LoginView.as_view(), name='login'), path('logout/', LogoutView.as_view(), name='logout')]
+
